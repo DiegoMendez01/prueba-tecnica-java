910df977385524c814222b1faed008b52774b671
DiegoMendez01 <diegomendez01soft@gmail.com>
2025-05-29 15:32:56 -0500

    Feat: List PlayList

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PlaylistFormComponent } from './components/playlist-form/playlist-form.component';
+import { PlaylistListComponent } from './components/playlist-list/playlist-list.component';
 
 export const routes: Routes = [
   { path: 'create', component: PlaylistFormComponent },
+  { path: 'lists', component: PlaylistListComponent },
 ];
--- frontend/src/app/components/playlist-list/playlist-list.component.ts
+++ frontend/src/app/components/playlist-list/playlist-list.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Playlist } from '../../models/playlist.model';
+import { PlaylistService } from '../../services/playlist.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-playlist-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './playlist-list.component.html',
-  styleUrl: './playlist-list.component.css'
+  styleUrl: './playlist-list.component.css',
 })
-export class PlaylistListComponent {
+export class PlaylistListComponent implements OnInit {
+  playlists: Playlist[] = [];
+  filteredPlaylists: Playlist[] = [];
+  filterText: string = '';
+  errorMessage: string = '';
 
+  constructor(private playlistService: PlaylistService) {}
+
+  ngOnInit(): void {
+    this.loadPlaylists();
+  }
+
+  loadPlaylists() {
+    this.playlistService.getAll().subscribe({
+      next: (data) => {
+        this.playlists = data;
+        this.applyFilter();
+      },
+      error: () => {
+        this.errorMessage = 'Error al cargar las playlists.';
+      },
+    });
+  }
+
+  deletePlaylist(name?: string) {
+    if (!name) return;
+
+    if (!confirm('¿Está seguro de eliminar esta playlist?')) {
+      return;
+    }
+
+    this.playlistService.delete(name).subscribe({
+      next: () => {
+        this.loadPlaylists();
+      },
+      error: () => {
+        this.errorMessage = 'Error al eliminar la playlist.';
+      },
+    });
+  }
+
+  applyFilter() {
+    const filter = this.filterText.toLowerCase();
+    this.filteredPlaylists = this.playlists.filter((p) =>
+      p.name.toLowerCase().includes(filter)
+    );
+  }
 }
